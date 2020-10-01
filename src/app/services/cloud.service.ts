e51333a80807f8f6402e889495bc6d2eb8e2a940
Son Dang <dhuyson@tma.com.vn>
2020-10-01 09:49:41 +0700

    new deploy

--- src/app/services/cloud.service.ts
+++ src/app/services/cloud.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from "@angular/core";
 import { of } from "rxjs";
+import Data from '../../assets/songs.json'
 
 const root = "../../assets/songs/"
 const deploy = true
@@ -10,23 +11,7 @@ const rootURL = 'https://raw.githubusercontent.com/slavril/boredradio/master/src
 })
   
 export class CloudService {
-  files: any = [
-    "perfect.mp3",
-//    "how you like that.mp3",
-//    "Bad guy.mp3",
-    "fell it still.mp3",
-    "i love you 3000.mp3",
-    'Wont go home without you.m4a',
-    "I Want To Hold Your Hand.mp3",
-    "You Are My Sunshine__Johnny Cash__320.mp3",
-    "Don_t Let Me Go - The Click Five [Lossless_FLAC].flac",
-    "I Love You - Avril Lavigne [Lossless_FLAC].flac",
-    "tat nhat lang ruc ro.mp3",
-    "Vi yeu cu dam dau remix.mp3",
-    "anh oi o lai.mp3",
-    "nguoi nao hay.mp3",
-    "Ngay Chua Giong Bao - Bui Lan Huong.mp3"
-  ];
+  files: any = Data.data
 
   getUrl() {
     let newArr = this.files.map((e) => {
