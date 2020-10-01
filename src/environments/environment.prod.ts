f8a769b3ea676978b2ca9c409f07eaab974cf3c0
Son Dang <dhuyson@tma.com.vn>
2020-10-01 16:28:23 +0700

    new version

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyAPPKidliGRmd3ez-oulfbi9094jOYt_1E",
+    authDomain: "boredradio-74f03.firebaseapp.com",
+    databaseURL: "https://boredradio-74f03.firebaseio.com",
+    projectId: "boredradio-74f03",
+    storageBucket: "boredradio-74f03.appspot.com",
+    messagingSenderId: "41787347058",
+    appId: "1:41787347058:web:4f547ef6dfec8b3a0632eb"
+  }
 };
